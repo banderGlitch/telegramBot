96697e6811d9a79be1149813b0a3999766dc4357
banderGlitch <nernaykumar98@gmail.com>
2024-08-09 18:23:29 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [theme, colorMode] = useMode();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+         <CssBaseline/>
+         <div className="app">
+          <main className="content">
+           <Topbar/>
+            <Routes>
+              <Route path="/"/>
+              {/* <Route path="/team"/>
+              <Route path="/contacts"/>
+              <Route path="/invoices"/>
+              <Route path="/form"/>
+              <Route path="/bar"/>
+              <Route path="/pie"/>
+              <Route path="*"/> */}
+            </Routes>
+          </main>
+         </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+
     </>
   )
 }
